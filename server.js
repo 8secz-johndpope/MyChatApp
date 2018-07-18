b364dbfaeccc04ae10e04525db9b29b9d761c521
BeoHoang <beohoang98@users.noreply.github.com>
2018-07-18 12:19:39 +0700

    update ui

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const session = require('express-session');
+const bodyparser = require('body-parser');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const db = require('mongodb');
 
 http.listen(80, ()=>{
 	console.log('Server is listen on port 80');
@@ -12,12 +15,59 @@ app.use(express.static(__dirname + '/public'));
 //library
 app.use('/socketio', express.static(__dirname + '/node_modules/socket.io-client/dist'));
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'))
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended: true}))
+app.set('view engine', 'ejs');
 
+//session
+app.use(session({
+	secret: "bethany",
+	cookie: {
+		"maxAge": 3600*24*30
+	}
+}))
 
 //index
 app.get('/', (req, res)=>{
-	res.sendFile(__dirname + '/index.html');
+	if (req.session && req.session.sid)
+	{
+		if (req.session.sid == "asd")
+		{
+			const user = {
+				avatar_src: '/image/test.png',
+				name: 'Admin',
+				liked: 100
+			}
+			res.render('home', {user: user});
+		}
+			
+
+		return;
+	}
+
+	res.redirect('/login');
 });
+//login page
+app.get('/login', (req, res)=>{
+	if (req.session.sid == 'asd') {
+		res.redirect('/');
+	}
+
+	res.render('login', {err: undefined});
+})
+app.post('/login', (req, res)=>{
+	const username = req.body.username;
+	const password = req.body.password;
+
+	if (username == 'admin' && password == 'admin')
+	{
+		req.session.sid = 'asd';
+		res.redirect('/');
+	}
+	else {
+		res.render('login', {err: 'login failed'})
+	}
+})
 
 //socket io
 
